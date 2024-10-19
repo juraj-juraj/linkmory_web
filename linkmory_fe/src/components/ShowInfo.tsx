1f302599087ea1192e42134cf29deea218ad3e22
Juraj Novosad <juraj.novosad@phonexia.com>
2024-10-20 00:54:28 +0200

    ready for prod

--- linkmory_fe/src/components/ShowInfo.tsx
+++ linkmory_fe/src/components/ShowInfo.tsx
@@ -9,20 +9,6 @@ interface props{
     user_info: userInfo;
 }
 
-function getLinkedInId(url: string) {
-    // Create a new URL object
-    const urlObj = new URL(url);
-    
-    // Get the pathname from the URL
-    const path = urlObj.pathname; // This will give you "/in/juraj-novosad-094765263/"
-    
-    // Split the path by '/' and get the last part
-    const parts = path.split('/');
-    const id = parts[parts.length - 2]; // The ID is the second last part
-    
-    return id; // Return the extracted ID
-}
-
 function ShowInfo({user_info}: props) {
     // const userAgent = navigator.userAgent.toLowerCase();
     // let device = "Desktop"
@@ -33,14 +19,13 @@ function ShowInfo({user_info}: props) {
     //     console.log('Android');
     //     device = "Android";
     // }
-
     return (
         <div className="show-info-main">
             <h1 className="heading" >{user_info.name}</h1>
             <p className="bio"> {user_info.bio}</p>
-            <a href={"fb://profile/" + user_info.id_fb} > <button className="contact-button facebook-bg"><img src={fb_logo}/>Get Contact</button></a>
-            <a href={"linkedin://in/" + getLinkedInId(user_info.link_linkedin)} > <button className="contact-button linkedin-bg"><img src={linkedin_logo}/>Get Contact</button></a>
-            <a href={"https://instagram.com/" + user_info.link_insta}> <button className="contact-button insta-bg"><img src={insta_logo}/>Get Contact</button></a>
+            {user_info.link_insta ? <a href={"https://instagram.com/" + user_info.link_insta}> <button className="contact-button insta-bg"><img src={insta_logo}/>Get Contact</button></a> : <></>}
+            {user_info.link_fb ?  <a href={"fb://profile/" + user_info.id_fb} > <button className="contact-button facebook-bg"><img src={fb_logo}/>Get Contact</button></a> : <></>}
+            {user_info.link_linkedin ? <a href={user_info.link_linkedin} > <button className="contact-button linkedin-bg"><img src={linkedin_logo}/>Get Contact</button></a> : <></>}
         </div>
     );
 }
