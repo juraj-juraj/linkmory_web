c8f804b48f97a64672069354fbc6e2fa2a22bf3c
Juraj Novosad <juraj.novosad@phonexia.com>
2024-12-16 22:05:17 +0100

    prepare backedn for list of met people

--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,7 @@
 from typing import Optional
 import requests
 import re
+from datetime import datetime
 
 import logging
 from fastapi import FastAPI, HTTPException, status, Request
@@ -16,6 +17,30 @@ class UserInfo(BaseModel):
     link_linkedin: Optional[str]
     id_fb: Optional[str]
     link_website: Optional[str]
+    tel_number: Optional[str]
+    email_address: Optional[str]
+    user_created: Optional[int] = None  # seconds since epoch
+    last_change: Optional[int] = None  # seconds since epoch
+
+
+class UserInfoDump(UserInfo):
+    id: str
+
+
+class UserConnection(BaseModel):
+    id_other: str
+    connection_created: Optional[int] = None  # seconds since epoch
+    note: Optional[str]
+
+
+class UserConnectionDump(UserConnection):
+    id: str
+
+
+class DataDump(BaseModel):
+    users: list[UserInfoDump]
+    connections: list[UserConnectionDump]
+
 
 class createResponse(BaseModel):
     return_message: str
@@ -26,6 +51,7 @@ class UserExists(BaseModel):
 
 
 users: dict[str, UserInfo] = dict()
+connections: dict[str, list[UserConnection]] = dict()
 
 logging.basicConfig(level=logging.INFO)
 app = FastAPI()
@@ -56,30 +82,38 @@ app.add_middleware(
 @app.post("/api/user/create/")
 async def create_user(id: str, request: UserInfo) -> createResponse:
     global users
+    # Delete user if sent with blank name
     if not request.name:
         if id in users:
             del users[id]
             logging.info(f"Deleting user {id}")
             return createResponse(return_message=f"User {id} deleted successfully")
         logging.info(f"Id {id} not created")
-        return createResponse(return_message=f"User {id} did not create")
+        return createResponse(return_message=f"User {id} does not exist")
 
     logging.info(f"Creating id: {id}")
-    if request.link_insta and (
-        request.link_insta.startswith("https://www.instagram.com/")
-        or request.link_insta.startswith("https://instagram.com/")
-    ):
-        request.link_insta = re.sub(
-            r"https://(www\.)?instagram\.com/", "", request.link_insta
-        ).split("/")[0]
-    
+    if request.link_insta:
+        if request.link_insta.startswith(
+            "https://www.instagram.com/"
+        ) or request.link_insta.startswith("https://instagram.com/"):
+            request.link_insta = re.sub(
+                r"https://(www\.)?instagram\.com/", "", request.link_insta
+            ).split("/")[0]
+        request.link_insta = request.link_insta.replace("@", "", 1)
+
     if request.link_website:
-        if request.link_website.startswith("https://") or request.link_website.startswith("http://"):
+        if request.link_website.startswith("https://") or request.link_website.startswith(
+            "http://"
+        ):
             pass  # URL is already correctly prefixed
         elif request.link_website.startswith("www."):
             request.link_website = f"https://{request.link_website}"
         else:
             request.link_website = f"https://www.{request.link_website}"
+
+    if id not in users:
+        request.user_created = int(datetime.now().timestamp())
+    request.last_change = int(datetime.now().timestamp())
 
     users[id] = request
     return createResponse(return_message=f"User {id} created successfully")
@@ -100,6 +134,67 @@ async def get_user(id: str) -> UserInfo:
             if match:
                 user.id_fb = match.group(1)
     return user
+
+
+@app.get("/api/admin/data_dump/")
+async def dump_data() -> DataDump:
+    global users
+    global connections
+    user_list = [UserInfoDump(id=id_user, **user.model_dump()) for id_user, user in users.items()]
+    connection_list = [
+        UserConnectionDump(id=id_user, **connection.model_dump())
+        for id_user, connections in connections.items()
+        for connection in connections
+    ]
+    return DataDump(users=user_list, connections=connection_list)
+
+
+@app.post("/api/admin/load_data/")
+async def load_data(data: DataDump) -> createResponse:
+    global users
+    global connections
+
+    for user in data.users:
+        users[user.id] = UserInfo(
+            **{k: v for k, v in user.model_dump().items() if k in UserInfo.model_fields.keys()}
+        )
+    for connection in data.connections:
+        connections[connection.id].append(
+            UserConnection(
+                **{
+                    k: v
+                    for k, v in connection.model_dump().items()
+                    if k in UserConnection.model_fields.keys()
+                }
+            )
+        )
+    return createResponse(return_message="Data loaded successfully")
+
+
+@app.post("/api/user/connection/")
+async def create_connection(id: str, request: UserConnection) -> createResponse:
+    global connections
+    print("In create_connection")
+    if id not in users:
+        raise HTTPException(status_code=404, detail=f"User {id} does not exist")
+    if request.id_other not in users:
+        raise HTTPException(status_code=404, detail=f"User {request.id_other} does not exist")
+    request.connection_created = int(datetime.now().timestamp())
+    if id not in connections:
+        connections[id] = [request]
+    else:
+        connections[id].append(request)
+    return createResponse(return_message=f"Connection created successfully")
+
+
+@app.get("/api/user/connections/")
+async def get_connections(id: str) -> list[UserConnection]:
+    global connections
+    print("In get_connections")
+    if id not in users:
+        raise HTTPException(status_code=404, detail=f"User {id} does not exist")
+    user_connections = connections[id] if id in connections else []
+    return user_connections
 
 
 @app.get("/api/user/exist/")
