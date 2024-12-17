d5f6291ef6175a78337c072d3615b177d17aa4fb
Juraj Novosad <juraj.novosad@phonexia.com>
2024-12-17 10:59:14 +0100

    implement list connections

--- linkmory_fe/src/components/listConnections.tsx
+++ linkmory_fe/src/components/listConnections.tsx
@@ -6,7 +6,7 @@ import {UserConnection} from "../models/userModel"
 import "./listConnections.css"
 
 interface props{
-    user_id: string;
+    user_id: string | null;
 }
 
 function ListConnections({user_id}: props) {
@@ -15,29 +15,39 @@ function ListConnections({user_id}: props) {
     useEffect(() => {
         const api = async() => {
             const data = await fetch(config.bUrl + "/user/connection/list/?id=" + user_id, {method: "GET"});
+            if(data.status === 404){
+                return
+            }
             const data_json = await data.json();
             const userConnections: UserConnection[] = data_json;
             setConnectionList(userConnections);
         };
-        api();
+        if (user_id) {
+            api();
+        }
     }, [user_id]);
 
     function handleClick(id: string) {
         window.location.href = "/?id=" + id;
         
     }
+    if (connectionList.length === 0) {
+        return null;
+    }
+
     return (
         <div className="list-connections-main">
-            <h2>Connections</h2>
+            <h2 className="heading">My Connections</h2>
             <div className="connection-list">
                 {connectionList.map((connection) => (
-                    <div>
-                    <p key={connection.id_other}>
-                        {connection.person_name}
-                    </p>
-                    <button className="visit-profile-button" key={connection.id_other} onClick={() => handleClick(connection.id_other)}>
-                        Visit profile
-                    </button>
+                    <div className="item" key={connection.id_other}>
+                        <img src="https://cdn.builder.io/api/v1/image/assets/TEMP/670fe62f49123097f1c74007524d0b953da1dd440a037cd4d654646d6e7d3feb?placeholderIfAbsent=true&apiKey=f560b18130354807b388ec0c9e912c6d" alt="" className="list-delimiter-line" />
+                        <p>
+                            {connection.person_name}
+                        </p>
+                        <button className="contact-button web-bg " onClick={() => handleClick(connection.id_other)}>
+                            Visit profile
+                        </button>
                     </div>
                 ))}
             </div>
